7486c2ad889332a542ae95ffd44d9ef79de7aec0
Noritada Kobayashi <noritada.kobayashi@gmail.com>
2024-10-05 16:39:12 +0900

    Cease warnings for the FFI bindings

--- aec-sys/src/lib.rs
+++ aec-sys/src/lib.rs
@@ -1,16 +1,17 @@
-use std::env;
+pub mod binding {
+    #![allow(non_camel_case_types)]
+    include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
-include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+    #[cfg(test)]
+    mod tests {
+        use super::*;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn aec_binding_works() {
-        assert_eq!(
-            (AEC_VERSION_MAJOR, AEC_VERSION_MINOR, AEC_VERSION_PATCH),
-            (1, 1, 3)
-        );
+        #[test]
+        fn aec_binding_works() {
+            assert_eq!(
+                (AEC_VERSION_MAJOR, AEC_VERSION_MINOR, AEC_VERSION_PATCH),
+                (1, 1, 3)
+            );
+        }
     }
 }
