c63b5842f1cc5c59e0f0532f91cd1a81210a5a35
Noritada Kobayashi <noritada.kobayashi@gmail.com>
2024-10-06 01:43:21 +0900

    Make the wrapper work correctly

--- aec-sys/src/lib.rs
+++ aec-sys/src/lib.rs
@@ -28,23 +28,22 @@ impl Stream {
     }
 
     pub fn encode(&mut self, input: &[u8], output: &mut [u8]) -> Result<(), &'static str> {
-        let mut stream = self.0;
-        stream.next_in = input.as_ptr();
-        stream.avail_in = input.len();
-        stream.next_out = output.as_mut_ptr();
-        stream.avail_out = output.len();
+        self.0.next_in = input.as_ptr();
+        self.0.avail_in = input.len();
+        self.0.next_out = output.as_mut_ptr();
+        self.0.avail_out = output.len();
 
-        let result = unsafe { aec_encode_init(&mut stream) };
+        let result = unsafe { aec_encode_init(&mut self.0) };
         if result as u32 != AEC_OK {
             return Err("aec_encode_init() failed");
         }
 
-        let result = unsafe { aec_encode(&mut stream, AEC_FLUSH as i32) };
+        let result = unsafe { aec_encode(&mut self.0, AEC_FLUSH as i32) };
         if result as u32 != AEC_OK {
             return Err("aec_encode() failed");
         }
 
-        let result = unsafe { aec_encode_end(&mut stream) };
+        let result = unsafe { aec_encode_end(&mut self.0) };
         if result as u32 != AEC_OK {
             return Err("aec_encode_end() failed"); // FIXME: support incomplete processing
         }
@@ -53,23 +52,22 @@ impl Stream {
     }
 
     pub fn decode(&mut self, input: &[u8], output: &mut [u8]) -> Result<(), &'static str> {
-        let mut stream = self.0;
-        stream.next_in = input.as_ptr();
-        stream.avail_in = input.len();
-        stream.next_out = output.as_mut_ptr();
-        stream.avail_out = output.len();
+        self.0.next_in = input.as_ptr();
+        self.0.avail_in = input.len();
+        self.0.next_out = output.as_mut_ptr();
+        self.0.avail_out = output.len();
 
-        let result = unsafe { aec_decode_init(&mut stream) };
+        let result = unsafe { aec_decode_init(&mut self.0) };
         if result as u32 != AEC_OK {
             return Err("aec_decode_init() failed");
         }
 
-        let result = unsafe { aec_decode(&mut stream, AEC_FLUSH as i32) };
+        let result = unsafe { aec_decode(&mut self.0, AEC_FLUSH as i32) };
         if result as u32 != AEC_OK {
             return Err("aec_decode() failed");
         }
 
-        let result = unsafe { aec_decode_end(&mut stream) };
+        let result = unsafe { aec_decode_end(&mut self.0) };
         if result as u32 != AEC_OK {
             return Err("aec_decode_end() failed"); // FIXME: support incomplete processing
         }
@@ -103,7 +101,7 @@ mod tests {
 
         let bits_per_sample = 4;
         let block_size = 16;
-        let rsi = 1;
+        let rsi = 128;
         let flags = AEC_DATA_MSB;
 
         let mut stream = Stream::new(bits_per_sample, block_size, rsi, flags);
@@ -123,7 +121,6 @@ mod tests {
         let result = stream.decode(&encoded, &mut decoded);
         assert!(result.is_ok());
 
-        let decoded = &decoded[..stream.total_out()];
-        assert_eq!(decoded, input);
+        assert_eq!(&decoded[..stream.total_out()], &input[..stream.total_out()]);
     }
 }
