b2c4fab3fbb75861b9faf708a1bcd1920e948fa2
Noritada Kobayashi <noritada.kobayashi@gmail.com>
2025-07-30 23:29:18 +0900

    Export only generated APIs to make it simple for the time being

--- libaec-sys/src/lib.rs
+++ libaec-sys/src/lib.rs
@@ -1,78 +1,78 @@
 #![allow(non_camel_case_types, unused)]
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
-pub struct Stream(aec_stream);
-
-impl Stream {
-    pub fn new(bits_per_sample: u32, block_size: u32, rsi: u32, flags: u32) -> Self {
-        let mut raw: aec_stream = unsafe { std::mem::zeroed() };
-        raw.bits_per_sample = bits_per_sample;
-        raw.block_size = block_size;
-        raw.rsi = rsi;
-        raw.flags = flags;
-        Self(raw)
-    }
-
-    pub fn total_in(&self) -> usize {
-        self.0.total_in
-    }
-
-    pub fn total_out(&self) -> usize {
-        self.0.total_out
-    }
-
-    pub fn encode(&mut self, input: &[u8], output: &mut [u8]) -> Result<(), &'static str> {
-        self.0.next_in = input.as_ptr();
-        self.0.avail_in = input.len();
-        self.0.next_out = output.as_mut_ptr();
-        self.0.avail_out = output.len();
-
-        let result = unsafe { aec_encode_init(&mut self.0) };
-        if result as u32 != AEC_OK {
-            return Err("aec_encode_init() failed");
-        }
-
-        let result = unsafe { aec_encode(&mut self.0, AEC_FLUSH as i32) };
-        if result as u32 != AEC_OK {
-            return Err("aec_encode() failed");
-        }
-
-        let result = unsafe { aec_encode_end(&mut self.0) };
-        if result as u32 != AEC_OK {
-            return Err("aec_encode_end() failed"); // FIXME: support incomplete processing
-        }
-
-        Ok(())
-    }
-
-    pub fn decode(&mut self, input: &[u8], output: &mut [u8]) -> Result<(), &'static str> {
-        self.0.next_in = input.as_ptr();
-        self.0.avail_in = input.len();
-        self.0.next_out = output.as_mut_ptr();
-        self.0.avail_out = output.len();
-
-        let result = unsafe { aec_decode_init(&mut self.0) };
-        if result as u32 != AEC_OK {
-            return Err("aec_decode_init() failed");
-        }
-
-        let result = unsafe { aec_decode(&mut self.0, AEC_FLUSH as i32) };
-        if result as u32 != AEC_OK {
-            return Err("aec_decode() failed");
-        }
-
-        let result = unsafe { aec_decode_end(&mut self.0) };
-        if result as u32 != AEC_OK {
-            return Err("aec_decode_end() failed"); // FIXME: support incomplete processing
-        }
-
-        Ok(())
-    }
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    struct Stream(aec_stream);
+
+    impl Stream {
+        fn new(bits_per_sample: u32, block_size: u32, rsi: u32, flags: u32) -> Self {
+            let mut raw: aec_stream = unsafe { std::mem::zeroed() };
+            raw.bits_per_sample = bits_per_sample;
+            raw.block_size = block_size;
+            raw.rsi = rsi;
+            raw.flags = flags;
+            Self(raw)
+        }
+
+        fn total_in(&self) -> usize {
+            self.0.total_in
+        }
+
+        fn total_out(&self) -> usize {
+            self.0.total_out
+        }
+
+        fn encode(&mut self, input: &[u8], output: &mut [u8]) -> Result<(), &'static str> {
+            self.0.next_in = input.as_ptr();
+            self.0.avail_in = input.len();
+            self.0.next_out = output.as_mut_ptr();
+            self.0.avail_out = output.len();
+
+            let result = unsafe { aec_encode_init(&mut self.0) };
+            if result as u32 != AEC_OK {
+                return Err("aec_encode_init() failed");
+            }
+
+            let result = unsafe { aec_encode(&mut self.0, AEC_FLUSH as i32) };
+            if result as u32 != AEC_OK {
+                return Err("aec_encode() failed");
+            }
+
+            let result = unsafe { aec_encode_end(&mut self.0) };
+            if result as u32 != AEC_OK {
+                return Err("aec_encode_end() failed"); // FIXME: support incomplete processing
+            }
+
+            Ok(())
+        }
+
+        fn decode(&mut self, input: &[u8], output: &mut [u8]) -> Result<(), &'static str> {
+            self.0.next_in = input.as_ptr();
+            self.0.avail_in = input.len();
+            self.0.next_out = output.as_mut_ptr();
+            self.0.avail_out = output.len();
+
+            let result = unsafe { aec_decode_init(&mut self.0) };
+            if result as u32 != AEC_OK {
+                return Err("aec_decode_init() failed");
+            }
+
+            let result = unsafe { aec_decode(&mut self.0, AEC_FLUSH as i32) };
+            if result as u32 != AEC_OK {
+                return Err("aec_decode() failed");
+            }
+
+            let result = unsafe { aec_decode_end(&mut self.0) };
+            if result as u32 != AEC_OK {
+                return Err("aec_decode_end() failed"); // FIXME: support incomplete processing
+            }
+
+            Ok(())
+        }
+    }
 
     #[test]
     fn aec_encoding_and_decoding_works() {
